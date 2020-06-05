a205f7fa964239015a256b481e034523014215e3
Victor Mudaki Mwachi <mudaki@localhost.localdomain>
2020-06-04 23:27:18 -0400

    articles app addition

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import HomePageView
 
 urlpatterns = [
-    url(r'^',HomePageView.as_view(),name='home'),
+    path('',HomePageView.as_view(),name='home'),
 ]
