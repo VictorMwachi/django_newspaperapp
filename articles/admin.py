8eb26ab3ae23a2cbbd2464f8ba1cd86e7793d2e8
Victor Mudaki Mwachi <mudaki@localhost.localdomain>
2020-06-08 22:33:34 -0400

    comments part

--- articles/admin.py
+++ articles/admin.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Article
+from .models import Article,Comment
 
+class CommentInLine(admin.StackedInline):
+    model = Comment
 
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -9,29 +11,42 @@ from .models import Article
 
 # Create your views here.
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model=Article
     template_name = 'article_new.html'
     fields = ('title','body',)
+    login_url = 'login'
 
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model = Article
     fields = ('title','body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
